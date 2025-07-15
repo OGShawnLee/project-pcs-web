35711cc25bef633c8be1bf94a760cb073ffa92e0
Shawn <changoloco.evg@gmail.com>
2025-07-15 01:56:23 +0000

    fix(gui): correct text in header description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 	<main class="max-w-xl w-full mx-auto flex flex-col border border-neutral-900">
 		<header class="p-8 grid gap-2 border-b border-neutral-900">
 			<h1 class="text-2xl text-white font-semibold tracking-tight">ProTrack</h1>
-			<p class="text-neutral-400 text-sm">Sistema de Gestor de Prácticas Profesionales</p>
+			<p class="text-neutral-400 text-sm">Sistema Gestor de Prácticas Profesionales</p>
 		</header>
 		<div class="p-8">
 			<a class="button button--main" href="/auth/sign-in"> Iniciar Sesión </a>
